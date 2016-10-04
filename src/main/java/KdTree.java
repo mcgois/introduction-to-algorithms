4b0bfa9b5d7d4357908c02ddb50ef3a3c49316c2
Matheus Cabral <matheus.cabral@gmail.com>
2016-10-04 15:45:29 -0300

    KdTree. Evolution.

--- src/main/java/KdTree.java
+++ src/main/java/KdTree.java
@@ -7,25 +7,54 @@ import java.util.TreeSet;
 
 public class KdTree {
 
-    private TreeSet<Point2D> points;
+    private Node root;
+    private int size;
+
+    private static class Node {
+        private Point2D p;
+        private RectHV rect;
+        private Node lb;
+        private Node rt;
+
+        Node(Point2D p) {
+            this.p = p;
+        }
+    }
 
     public KdTree() {
-        this.points = new TreeSet<>();
+        this.root = null;
+        this.size = 0;
     }
 
     public boolean isEmpty() {
-        return this.points.isEmpty();
+        return root != null;
     }
 
     public int size() {
-        return this.points.size();
+        return size;
     }
 
     public void insert(Point2D p) {
         checkArguments(p);
-        if (!this.points.contains(p)) {
-            this.points.add(p);
+        // if (!contains(p)) {
+        this.root = put(this.root, p, true);
+        this.size++;
+    }
+
+    private Node put(Node h, Point2D p, boolean xOrientation) {
+        // if leaf
+        if (h == null) {
+            return new Node(p);
         }
+
+        // comparison
+        double cmp = xOrientation ? p.x() - h.p.x(): p.y() - h.p.y();
+
+        if (cmp < 0) h.lb = put(h.lb, p, !xOrientation);
+        else if (cmp > 0) h.rt = put(h.rt, p, !xOrientation);
+        else if (cmp == 0) h.p = p;
+
+        return h;
     }
 
     private void checkArguments(Object p) {
@@ -36,37 +65,36 @@ public class KdTree {
 
     public boolean contains(Point2D p) {
         checkArguments(p);
-        return this.points.contains(p);
+        return contains(this.root, p, true);
+    }
+
+    private boolean contains(Node h, Point2D p, boolean xOrientation) {
+        if (h == null) {
+            return false;
+        }
+
+        if (h.p.equals(p)) {
+            return true;
+        }
+
+        // comparison
+        double cmp = xOrientation ? p.x() - h.p.x(): p.y() - h.p.y();
+
+        if (cmp < 0) return contains(h.lb, p, !xOrientation);
+        else return contains(h.rt, p, !xOrientation);
     }
 
     public void draw() {
-        for (Point2D p : this.points) {
-            p.draw();
-        }
     }
 
     public Iterable<Point2D> range(RectHV rect) {
         checkArguments(rect);
-        List<Point2D> result = new ArrayList<>();
-        for (Point2D p : this.points) {
-            if (rect.contains(p)) {
-                result.add(p);
-            }
-        }
-        return result;
+        return null;
     }
 
     public Point2D nearest(Point2D p) {
         checkArguments(p);
-        double min = Double.MAX_VALUE;
-        Point2D minPoint = null;
-        for (Point2D point : this.points) {
-            if (point.distanceSquaredTo(p) < min) {
-                min = point.distanceSquaredTo(p);
-                minPoint = point;
-            }
-        }
-        return minPoint;
+        return null;
     }
 
     public static void main(String[] args) {
@@ -77,6 +105,6 @@ public class KdTree {
         pointSET.insert(new Point2D(0.4, 0.7));
         pointSET.insert(new Point2D(0.9, 0.6));
 
-
+        System.out.println(pointSET.contains(new Point2D(0.2, 0.3)));
     }
 }
